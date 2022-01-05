858b49d18ebfd05e693677320a0f6c2201212a3a
Yateng Hong <toothache9010@gmail.com>
2022-01-05 11:03:12 +0800

    Update ort.ps1
    Take latest ORT release version.

--- win/pkgs/ort.ps1
+++ win/pkgs/ort.ps1
@@ -29,9 +29,6 @@ $latest_ver = 'v' + $($(git ls-remote --tags "$repo") -match '.*refs/tags/v[0-9\
 # Use master.
 # $latest_ver = 'master'
 
-# Use user branch
-$latest_ver = 'tracysh/convsym'
-
 git clone --recursive -b "$latest_ver" -j100 "$repo"
 pushd "$root"
 
